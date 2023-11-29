9b6f8a7471006a132a27e1acd189b0089aef3fe1
VaIOReTto1 <1302140648@qq.com>
2023-11-29 15:03:57 +0800

    feature：添加顶部搜索栏

--- lib/explore_page/search.dart
+++ lib/explore_page/search.dart
@@ -1,4 +1,5 @@
 import 'package:cloude_music/config/config.dart';
+import 'package:cloude_music/config/icon.dart';
 import 'package:flutter/material.dart';
 
 class Search extends StatefulWidget {
@@ -11,29 +12,52 @@ class Search extends StatefulWidget {
 class _SearchState extends State<Search> {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Icon(Icons.menu,size: UIConfig.appBarIconSize,),
-        Container(
-          width: 300,
-          height: 40,
-          decoration: const BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(65.0),),
-            color: Color(UIConfig.searchColor),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        children: [
+          Icon(
+            Icons.menu,
+            size: UIConfig.appBarIconSize * 1.2,
           ),
-          child: TextField(
-            cursorColor: const Color(UIConfig.searchColor),
-            style: const TextStyle(color: Colors.black),
-            decoration: InputDecoration(
-              contentPadding: const EdgeInsets.symmetric(
-                  horizontal: 0, vertical: 2.0),
-              hintText: "你是我为数不多的骄傲",
-              border: InputBorder.none,
-              prefixIcon: Icon(Icons.search,size: UIConfig.searchIconSize,),
+          const SizedBox(
+            width: 15,
+          ),
+          Container(
+            width: 300,
+            height: UIConfig.appBarIconSize * 1.5,
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(
+                Radius.circular(65.0),
+              ),
+              color: Color(UIConfig.searchColor),
+            ),
+            child: TextField(
+              cursorColor: const Color(UIConfig.searchColor),
+              style: const TextStyle(color: Colors.black),
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                  hintText: "你是我为数不多的骄傲",
+                  border: InputBorder.none,
+                  prefixIcon: Icon(
+                    Icons.search,
+                    size: UIConfig.searchIconSize*1.2,
+                  ),
+                  suffixIcon: Icon(
+                    MyIcons.QRCode,
+                    size: UIConfig.searchIconSize,
+                  )),
             ),
           ),
-        ),
-      ],
+          const SizedBox(
+            width: 15,
+          ),
+          Icon(
+            MyIcons.free,
+            size: UIConfig.appBarIconSize,
+          ),
+        ],
+      ),
     );
   }
 }
